1614f7b6eb93eeafb3d2378af098422f6397eec5
MuradMT <mammadzade03@gmail.com>
2024-12-24 20:54:41 +0000

    Finished exercises.

--- src/Exercises/ExerciseUtil.java
+++ src/Exercises/ExerciseUtil.java
@@ -1,13 +1,26 @@
 package Exercises;
 
-public class ExerciseUtil {
-    public class Task1{
+import java.util.Scanner;
+
+public  class ExerciseUtil {
+    public static class Task1{
         public static boolean isPrime(int n) {
             if (n <= 1) return false;
             for (int i = 2; i * i <= n; i++) {
                 if (n % i == 0) return false;
             }
             return true;
+        }
+
+
+    }
+    public static class Task2{
+        public static void fillArray(int[] arr) {
+               int size=arr.length;
+               var sc=new Scanner(System.in);
+               for(int i=0;i<size;i++){
+                   arr[i]=sc.nextInt();
+               }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 import Exercises.*;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ExerciseUtil.Task1.isPrime(5));
+        //Note.C# static class forces all member to be static,
+        //in java the situation is different
+        var num= new Scanner(System.in).nextInt();
+        for(int i=0;i<num;i++){
+            if(ExerciseUtil.Task1.isPrime(i))
+                System.out.println(i);
+        }
+        int[] arr =new int[num];
+        ExerciseUtil.Task2.fillArray(arr);
+        for(int i=0;i<arr.length;i++){
+            if(ExerciseUtil.Task1.isPrime(arr[i])){
+                System.out.println(arr[i]);
+            }
+        }
+
+
     }
 }
