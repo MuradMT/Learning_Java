ffdc751b841bc2feb8654a14317ee5e04729467d
MuradMT <mammadzade03@gmail.com>
2024-12-24 23:19:04 +0000

    Learned about class and class related topics such as static,field,object.Additionally,learned about pointer vs reference differences,JVM and RAM relationship, pointer implementation in C,essential parts of computer such as mother board,case ,power supply,cpu,ram,hard drive(solid state drive ssd and hard disk drive hdd),graphics card.

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public  String name;
+    public  String surname;
+    public  int age;
+
+    public static void print(Student sd){
+        System.out.println(sd.name);
+    }
+}
