1b2947728bf8d70839eec350cea69be914f863d9
MuradMT <mammadzade03@gmail.com>
2024-12-29 00:32:06 +0000

    Looked at static object,stack overflow example withobject initiation,object array,.getClass().getName() such as c# reflection,getter setter default versions,getter record style,setter builder pattern. Encapsulation is information,Abstraction is implementation hiding.

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,31 @@
 public class Main {
     /* Month-2
     Topics covered:
-    1.Pass by value(only with value types)
+     1.Static object
+     2.Stack overflow - object initiation in both main method and inside it
+     example:Student field in Student class and Student object initiation in main
+     3.Object array
+     4.C# reflection topic similarity such as getClass().getName()
+     5.Encapsulation-Allows you only to reach essential information not all information
+     general using example is dtos
+     Abstraction-Hides unnecessary details,focuses only to key things
+     Encapsulation-Information hiding
+     Abstraction-Implementation hiding
+     6.getter and setter methods and templates (record style getter,builder setter or default)
+     7.Builder pattern in setter method
      */
     public static void main(String[] args){
-          int a=4;
-          deyish(a);
-          System.out.println(a);
+         Student[] s={
+            new Student(),new Student(),new Student()
+        };
+         s[0].age=5;
+         s[1].age=6;
+         s[2].age=7;
+         for(var item:s){
+             System.out.println(item.age);
+         }
+         var st=new Student().setName("Murad").setSurname("Mammadzada");
+        System.out.println(st.name()+st.surname());
     }
 
-    public static void deyish(int a) {
-        a=5;
-    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,33 @@
 public class Student {
-    public  String name;
-    public  String surname;
-    public  int age;
+      String name;
+      String surname;
+      int age;
+
+    public String surname() {
+        return surname;
+    }
+
+    public Student setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public static void print(Student sd){
         System.out.println(sd.name);
