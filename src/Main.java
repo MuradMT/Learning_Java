3dd9bd49b6eb8ab96db059dc47205e39f82607b6
MuradMT <mammadzade03@gmail.com>
2025-01-29 00:06:13 +0000

    Looked at to type casting,string pool,equals method.Finished month 2.

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,16 @@
 public class Main {
     /* Month-2
     Topics covered:
-    1.Method overloading
-    2.Wrapper types
-    3.boxing,unboxing
-    4.New object creation after the value of Integer 127
+    1.Type casting
+    2.String Pool
+    3.equals
      */
     public static void main(String[] args) {
-         int i=0;
-         Integer j=null;
-         //Integer k=new Integer(i); deprecated code
-        Integer y=i; //auto-boxing
-        int z=y; //auto unboxing
-        var t= y.toString();
-        Byte b=0;
-        Long l=0l;
-        Character c=' ';
-        Boolean bb=true;
-        String name="Murad";//char array
-
-        Integer aa=128;
-        Integer ab=128;
-        //Until 127 ,they create same reference address
-        // aa!=ab
-        //aa=127 ab=127 aa==ab
+        int a=9;
+        String s=String.valueOf(a);//a+"";
+        var val=Integer.parseInt(s);
+        //coursera,leetcode
     }
-
-    public static int foo(int a, int b) {
-        return a+b;
-    }
-    public static double foo(double a, double b) {
-        return a+b;
-    }
-
 
 
 }
